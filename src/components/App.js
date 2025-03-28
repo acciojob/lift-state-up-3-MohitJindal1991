bbd715eee1ec7bf0cdb5231757a92b419622d811
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-03-28 16:43:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
+      <h1>Parent Component</h1>
+      <h3>Child Component</h3>
         {/* Do not remove the main div */}
     </div>
   )
