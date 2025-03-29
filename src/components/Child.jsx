10a4acad58708531cdd34e530bf444a46394880d
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-03-29 18:46:51 +0530

    Update Child.jsx

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,1 +1,11 @@
+import React from "react";
 
+export default function Child({num}){
+
+  return (
+    <div>
+       <p>Child Component {num}</p>
+      <button>Option {num}</button>
+    </div>
+  );
+}
