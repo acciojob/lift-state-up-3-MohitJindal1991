903db3cbeccf122f1c85ebc7135ff14a5559ca22
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-03-29 18:51:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [selected,setSelected]=useState("");
+  function clickHandler(num){
+    setSelected(num);
+  }
   return (
     <div>
       <h1>Parent Component</h1>
       <h3>Child Component</h3>
+      <Child handler={clickHandler} num={1}/>
+      <Child handler={clickHandler} num={2}/>
+    <p> Selected Option: Option {selected}</p>
         {/* Do not remove the main div */}
     </div>
   )
